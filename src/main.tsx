4197dba1ad228c008084ea0366af815da13e93f4
Surtin <dung862002@gmail.com>
2025-05-31 11:24:18 +0700

    feat: integrate Vercel Analytics for improved tracking

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import "./index.css";
 import { createRoot } from "react-dom/client";
 import { Route, Routes } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 // Pages
 import HomePage from "./pages/home/home.page";
 import CollisionPage from "./pages/collision/collision.page";
@@ -12,15 +13,18 @@ import Terrain3DPage from "./pages/terrain-3d/terrain-3d.page";
 import AsteroidsPage from "./pages/asteroids/asteroids.page";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/collision" element={<CollisionPage />} />
-      <Route path="/flocking" element={<FlockingPage />} />
-      <Route path="/cube-wave" element={<CubeWavePage />} />
-      <Route path="/raycasting" element={<RaycastingPage />} />
-      <Route path="/terrain-3d" element={<Terrain3DPage />} />
-      <Route path="/asteroids" element={<AsteroidsPage />} />
-    </Routes>
-  </BrowserRouter>
+  <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/collision" element={<CollisionPage />} />
+        <Route path="/flocking" element={<FlockingPage />} />
+        <Route path="/cube-wave" element={<CubeWavePage />} />
+        <Route path="/raycasting" element={<RaycastingPage />} />
+        <Route path="/terrain-3d" element={<Terrain3DPage />} />
+        <Route path="/asteroids" element={<AsteroidsPage />} />
+      </Routes>
+    </BrowserRouter>
+    <Analytics />
+  </>
 );
